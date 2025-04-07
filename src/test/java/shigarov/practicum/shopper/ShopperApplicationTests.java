8b70271cf7e4d8ad16da31b186f6ff0b5a51dc9a
Alexey O. Shigarov <shigarov@gmail.com>
2025-04-07 09:58:44 +0800

    init

---
+++ src/test/java/shigarov/practicum/shopper/ShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package shigarov.practicum.shopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
